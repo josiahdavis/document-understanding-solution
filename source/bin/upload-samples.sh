614338fdc64c4399457262e26f668cfa52434034
EC2 Default User <ec2-user@ip-172-31-36-131.ap-southeast-1.compute.internal>
2020-09-04 07:45:09 +0000

    remove samples docs, updated package with dev email

--- source/bin/upload-samples.sh
+++ source/bin/upload-samples.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 source .env
 aws s3 cp ./samples/ClassicMode/ s3://$SampleBucketName/public/samples/ --recursive
+
 if [ $ENABLE_KENDRA = "true" ]
 then
     aws s3 sync s3://$CovidDataBucketName/documents/GeneralPublic/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/Scientist/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+    #aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+    #aws s3 sync s3://$CovidDataBucketName/documents/Scientist/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
     aws s3 sync s3://$CovidDataBucketName/documents/GeneralPublic/pdf/  s3://$BulkProcessingBucketName/documentDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/pdf/  s3://$BulkProcessingBucketName/documentDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/Scientist/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    #aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    #aws s3 sync s3://$CovidDataBucketName/documents/Scientist/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    #aws s3 sync s3://$CovidDataBucketName/documents/Wikipedia/pdf/  s3://$BulkProcessingBucketName/documentDrop/
 fi
 
 
